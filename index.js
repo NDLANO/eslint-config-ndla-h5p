5a9e21ceccebdddf9066961af8abf302e60974df
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-11-21 16:58:55 +0100

    feat: initialize eslint rules

---
+++ index.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    rules: {
+      semi: [2, 'always'],
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'brace-style': ['error', 'stroustrup'],
+      'keyword-spacing': ['error', { after: true }],
+      'comma-spacing': ['error', { before: false, after: true }],
+      'space-infix-ops': ['error', { int32Hint: false }],
+      eqeqeq: [2, 'smart'],
+      'space-before-blocks': 'error',
+      'space-before-function-paren': [
+        'error',
+        {
+          anonymous: 'always',
+          named: 'never',
+          asyncArrow: 'always',
+        },
+      ],
+      'no-extra-boolean-cast': 'off',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'arrow-parens': ['error', 'always'],
+      'no-use-before-define': 'error',
+      'import/no-extraneous-dependencies': 'warn',
+    },
+  },
+];
