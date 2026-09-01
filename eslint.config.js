ddef30b339f11470a57e360f1ff0b2f4c8ff9612
Oliver Tacke <o.tacke@posteo.de>
2026-09-01 22:36:36 +0200

    Fix typescript linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
-import js from "@eslint/js";
-import globals from "globals";
-import importPlugin from "eslint-plugin-import-x";
-import stylisticJs from "@stylistic/eslint-plugin";
+import js from '@eslint/js';
+import globals from 'globals';
+import importPlugin from 'eslint-plugin-import-x';
+import stylisticJs from '@stylistic/eslint-plugin';
 
 const INDENTATION_CHARS = 2;
 
 export default [
-  { ...js.configs.recommended, files: ["**/*.js", "**/*.mjs", "**/*.cjs"] },
+  { ...js.configs.recommended, files: ['**/*.js', '**/*.mjs', '**/*.cjs'] },
   {
     ...importPlugin.flatConfigs.recommended,
     ...{
-      name: "eslint-config-ndla-h5p",
-      files: ["**/*.js"],
+      name: 'eslint-config-ndla-h5p',
+      files: ['**/*.js'],
       languageOptions: {
         parserOptions: {
           ecmaFeatures: {
@@ -20,53 +20,53 @@ export default [
         },
         globals: {
           ...globals.browser,
-          H5P: "readonly",
-          H5PEditor: "readonly",
-          H5PIntegration: "readonly",
+          H5P: 'readonly',
+          H5PEditor: 'readonly',
+          H5PIntegration: 'readonly',
         },
       },
       plugins: {
         import: importPlugin,
-        "@stylistic/js": stylisticJs,
+        '@stylistic/js': stylisticJs,
       },
       rules: {
         // H5P Group rules
-        "@stylistic/js/semi": ["error", "always"],
-        "@stylistic/js/indent": ["error", INDENTATION_CHARS, { SwitchCase: 1 }],
-        "@stylistic/js/brace-style": ["error", "stroustrup"],
-        "@stylistic/js/keyword-spacing": ["error", { after: true }],
-        "@stylistic/js/comma-spacing": [
-          "error",
+        '@stylistic/js/semi': ['error', 'always'],
+        '@stylistic/js/indent': ['error', INDENTATION_CHARS, { SwitchCase: 1 }],
+        '@stylistic/js/brace-style': ['error', 'stroustrup'],
+        '@stylistic/js/keyword-spacing': ['error', { after: true }],
+        '@stylistic/js/comma-spacing': [
+          'error',
           { before: false, after: true },
         ],
-        "@stylistic/js/space-infix-ops": ["error", { int32Hint: false }],
-        eqeqeq: ["error", "smart"],
-        "@stylistic/js/space-before-blocks": "error",
-        "@stylistic/js/space-before-function-paren": [
-          "error",
+        '@stylistic/js/space-infix-ops': ['error', { int32Hint: false }],
+        eqeqeq: ['error', 'smart'],
+        '@stylistic/js/space-before-blocks': 'error',
+        '@stylistic/js/space-before-function-paren': [
+          'error',
           {
-            anonymous: "always",
-            named: "never",
-            asyncArrow: "always",
+            anonymous: 'always',
+            named: 'never',
+            asyncArrow: 'always',
           },
         ],
-        "no-extra-boolean-cast": "off",
-        "no-console": ["error", { allow: ["warn", "error"] }],
-        "@stylistic/js/quotes": ["error", "single"],
-        "import/extensions": ["error", "ignorePackages", { js: "always" }],
-        "import/prefer-default-export": "off",
-        "no-restricted-syntax": "off",
-        "no-plusplus": "off",
-        "no-continue": "off",
+        'no-extra-boolean-cast': 'off',
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        '@stylistic/js/quotes': ['error', 'single'],
+        'import/extensions': ['error', 'ignorePackages', { js: 'always' }],
+        'import/prefer-default-export': 'off',
+        'no-restricted-syntax': 'off',
+        'no-plusplus': 'off',
+        'no-continue': 'off',
         // NDLA specific rules
-        "no-use-before-define": "error",
-        "import/no-extraneous-dependencies": "warn",
-        "@stylistic/js/arrow-parens": ["error", "always"],
-        "@stylistic/js/object-curly-spacing": ["error", "always"],
-        "no-alert": ["error"],
-        "no-eval": ["error"],
-        "no-implied-eval": ["error"],
-        "@stylistic/js/max-len": ["warn", { code: 120 }],
+        'no-use-before-define': 'error',
+        'import/no-extraneous-dependencies': 'warn',
+        '@stylistic/js/arrow-parens': ['error', 'always'],
+        '@stylistic/js/object-curly-spacing': ['error', 'always'],
+        'no-alert': ['error'],
+        'no-eval': ['error'],
+        'no-implied-eval': ['error'],
+        '@stylistic/js/max-len': ['warn', { code: 120 }],
       },
     },
   },
@@ -77,8 +77,8 @@ export default [
     // including typescript-eslint.
     ...importPlugin.flatConfigs.recommended,
     ...{
-      name: "eslint-config-ndla-h5p/ts",
-      files: ["**/*.ts", "**/*.tsx"],
+      name: 'eslint-config-ndla-h5p/ts',
+      files: ['**/*.ts', '**/*.tsx'],
       languageOptions: {
         parserOptions: {
           ecmaFeatures: {
@@ -87,63 +87,68 @@ export default [
         },
         globals: {
           ...globals.browser,
-          H5P: "readonly",
-          H5PEditor: "readonly",
-          H5PIntegration: "readonly",
+          H5P: 'readonly',
+          H5PEditor: 'readonly',
+          H5PIntegration: 'readonly',
         },
       },
       plugins: {
         import: importPlugin,
-        "@stylistic/ts": stylisticJs,
+        '@stylistic/ts': stylisticJs,
+      },
+      settings: {
+        'import-x/resolver': {
+          typescript: true,
+        },
       },
       rules: {
         // H5P Group rules
-        "@stylistic/ts/semi": ["error", "always"],
-        "@stylistic/ts/indent": ["error", INDENTATION_CHARS, { SwitchCase: 1 }],
-        "@stylistic/ts/brace-style": ["error", "stroustrup"],
-        "@stylistic/ts/keyword-spacing": ["error", { after: true }],
-        "@stylistic/ts/comma-spacing": [
-          "error",
+        '@stylistic/ts/semi': ['error', 'always'],
+        '@stylistic/ts/indent': ['error', INDENTATION_CHARS, { SwitchCase: 1 }],
+        '@stylistic/ts/brace-style': ['error', 'stroustrup'],
+        '@stylistic/ts/keyword-spacing': ['error', { after: true }],
+        '@stylistic/ts/comma-spacing': [
+          'error',
           { before: false, after: true },
         ],
-        "@stylistic/ts/space-infix-ops": ["error", { int32Hint: false }],
-        eqeqeq: ["error", "smart"],
-        "@stylistic/ts/space-before-blocks": "error",
-        "@stylistic/ts/space-before-function-paren": [
-          "error",
+        '@stylistic/ts/space-infix-ops': ['error', { int32Hint: false }],
+        eqeqeq: ['error', 'smart'],
+        '@stylistic/ts/space-before-blocks': 'error',
+        '@stylistic/ts/space-before-function-paren': [
+          'error',
           {
-            anonymous: "always",
-            named: "never",
-            asyncArrow: "always",
+            anonymous: 'always',
+            named: 'never',
+            asyncArrow: 'always',
           },
         ],
-        "no-extra-boolean-cast": "off",
-        "no-console": ["error", { allow: ["warn", "error"] }],
-        "@stylistic/ts/quotes": ["error", "single"],
-        "import/extensions": [
-          "error",
-          "ignorePackages",
-          { js: "always", ts: "never", tsx: "never" },
+        'no-extra-boolean-cast': 'off',
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+        '@stylistic/ts/quotes': ['error', 'single'],
+        'import/extensions': [
+          'error',
+          'ignorePackages',
+          { js: 'always', ts: 'never', tsx: 'never' },
         ],
-        "import/prefer-default-export": "off",
-        "no-restricted-syntax": "off",
-        "no-plusplus": "off",
-        "no-continue": "off",
+        'import/prefer-default-export': 'off',
+        'no-restricted-syntax': 'off',
+        'no-plusplus': 'off',
+        'no-continue': 'off',
         // NDLA specific rules
-        "no-use-before-define": "error",
-        "import/no-extraneous-dependencies": "warn",
-        "@stylistic/ts/arrow-parens": ["error", "always"],
-        "@stylistic/ts/object-curly-spacing": ["error", "always"],
-        "no-alert": ["error"],
-        "no-eval": ["error"],
-        "no-implied-eval": ["error"],
-        "@stylistic/ts/max-len": ["warn", { code: 120 }],
+        'no-use-before-define': 'error',
+        'import/no-extraneous-dependencies': 'warn',
+        '@stylistic/ts/arrow-parens': ['error', 'always'],
+        '@stylistic/ts/object-curly-spacing': ['error', 'always'],
+        'no-alert': ['error'],
+        'no-eval': ['error'],
+        'no-implied-eval': ['error'],
+        '@stylistic/ts/max-len': ['warn', { code: 120 }],
       },
     },
   },
   {
-    name: "eslint-config-ndla-h5p/node",
-    files: ["webpack.config.js", "eslint.config.js"],
+    name: 'eslint-config-ndla-h5p/node',
+    files: ['webpack.config.js', 'eslint.config.js'],
     languageOptions: {
       globals: {
         ...globals.node,
